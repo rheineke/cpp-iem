f0ae9e1cfda2b3dcec53e6315f452273f327180f
Reece Heineke <reece.heineke@gmail.com>
2016-10-22 10:24:34 -0400

    #3 Removed hardcoded absolute path to required config file

--- test/iem/contract_test.cpp
+++ test/iem/contract_test.cpp
@@ -1,15 +1,18 @@
 // Copyright 2016 Reece Heineke<reece.heineke@gmail.com>
+#include "boost/filesystem.hpp"
+
 #include "iem/contract.hpp"
 
 #include "gtest/gtest.h"
 
-// TODO: Ensure market constructor fails correctly on bad market name
+// TODO(rheineke): Ensure market constructor fails correctly on bad market name
 TEST(ContractTest, BadMarketNameException) {
-  iem::Market m("FedPolicyB", "/Users/rheineke/code/ciem/conf/markets.json");
+  std::cout << "Current path: " << boost::filesystem::current_path() << std::endl;
+  iem::Market m("FedPolicyB", "conf/markets.json");
   EXPECT_TRUE(m.value() == 51);
-  // EXPECT_ANY_THROW(iem::Market("FedPolicyC"));
+  EXPECT_ANY_THROW(iem::Market("FedPolicyC"));
   // EXPECT_THROW(iem::Market("FedPolicyC"), std::invalid_argument);
 }
 
-// TODO: Test throw "string" with struct implementing what()
-// TODO: Test contract and contract bundle in similar ways
+// TODO(rheineke): Test throw "string" with struct implementing what()
+// TODO(rheineke): Test contract and contract bundle in similar ways
