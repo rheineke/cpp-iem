b9aff094c3bc1d28028c69ebf3a55f3d020d6e9c
Reece Heineke <reece.heineke@gmail.com>
2017-02-01 07:11:37 -0500

    MAINT: Set env, packages in Travis YAML (#13)

--- .travis/before_install.sh
+++ .travis/before_install.sh
@@ -5,17 +5,14 @@ if [[ $TRAVIS_OS_NAME == 'osx' ]]; then
     brew upgrade
     <.travis/brew.txt xargs brew install
 elif [[ $TRAVIS_OS_NAME == 'linux' ]]; then
-  sudo add-apt-repository ppa:ubuntu-toolchain-r/test -y
-  sudo apt-get update -q
-  if [[ $CXX == 'clang++' ]]; then
-    sudo apt-get install -y clang-3.8
-    export CXX=/usr/bin/clang++-3.8
-    export CC=/usr/bin/clang-3.8
-  elif [[ $CXX == 'g++' ]]; then
-    sudo apt-get install -y gcc-6 g++-6
-    export CXX=/usr/bin/g++-6
-    export CC=/usr/bin/gcc-6
-  fi
-  sudo apt-get install -y libboost-all-dev openssl libssl-dev
-  sudo apt-get install -y cmake libcppnetlib-dev
+    echo "Foobar"
+#  sudo add-apt-repository ppa:ubuntu-toolchain-r/test -y
+#  sudo apt-get update -q
+#  if [[ $CXX == 'clang++' ]]; then
+#    sudo apt-get install -y clang-3.8
+#  elif [[ $CXX == 'g++' ]]; then
+#    sudo apt-get install -y gcc-6 g++-6
+#  fi
+#  sudo apt-get install -y libboost-all-dev openssl libssl-dev
+#  sudo apt-get install -y cmake libcppnetlib-dev
 fi
