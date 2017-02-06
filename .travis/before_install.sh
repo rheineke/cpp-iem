94cec8c32f9646009aca8cdd9aeb4e5285121308
Reece Heineke <reece.heineke@gmail.com>
2017-02-06 06:35:45 -0500

    MAINT: Install more recent boost as cpp-netlib does (#13)

--- .travis/before_install.sh
+++ .travis/before_install.sh
@@ -6,6 +6,8 @@ if [[ $TRAVIS_OS_NAME == 'osx' ]]; then
     <.travis/brew.txt xargs brew install
 elif [[ $TRAVIS_OS_NAME == 'linux' ]]; then
     sudo add-apt-repository ppa:ubuntu-toolchain-r/test -y
+    sudo add-apt-repository ppa:llvm-toolchain-precise-3.6
+    sudo add-apt-repository ppa:kalakris-cmake -y
     sudo apt-get update -q
     if [[ $CXX == 'clang++' ]]; then
       sudo apt-get install -y clang-3.8
