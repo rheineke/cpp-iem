6a87bace7e233afc05446d879572f9435131fea0
Reece Heineke <reece.heineke@gmail.com>
2017-02-09 06:25:35 -0500

    MAINT: Move packages to addons key in Travis YAML (#13)

--- .travis/before_install.sh
+++ .travis/before_install.sh
@@ -5,17 +5,6 @@ if [[ $TRAVIS_OS_NAME == 'osx' ]]; then
     brew upgrade
     <.travis/brew.txt xargs brew install
 elif [[ $TRAVIS_OS_NAME == 'linux' ]]; then
-    add-apt-repository ppa:ubuntu-toolchain-r/test -y
-    add-apt-repository ppa:llvm-toolchain-precise-3.6
-    add-apt-repository ppa:kalakris-cmake -y
-    apt-get update -q
-    if [[ $CXX == 'clang++' ]]; then
-      apt-get install -y clang-3.8
-    elif [[ $CXX == 'g++' ]]; then
-      apt-get install -y gcc-6 g++-6
-    fi
-    apt-get install -y libboost-all-dev openssl libssl-dev
-    apt-get install -y cmake libcppnetlib-dev
-
-    apt list --installed
+    echo "Nothing to do here"
+    # apt list --installed
 fi
